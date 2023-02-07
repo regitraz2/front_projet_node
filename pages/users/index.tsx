b8b52a1ff0176014d104b73f59fd8963946b9a42
regitraz2 <guillaume@subvitamine.com>
2023-02-07 17:25:42 +0100

    Update index.tsx

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
-import { IUser } from "@/interfaces/IUser";
+import {useEffect, useState} from "react";
+import {IUser} from "@/interfaces/IUser";
 import * as process from "process";
 import axios from "axios";
 import UserCard from "@/components/UserCard";
+import BaseLayout from "@/components/BaseLayout";
 
 const UserList = () => {
 
@@ -20,19 +21,19 @@ const UserList = () => {
 
     const setSearchCategoryUsers = async (category: string) => {
         if (category != "Toutes les catégories") {
-     
-                    if (category != "Toutes les catégories") {
 
-                        newFilterUsers = newUsers.filter((user: IUser) => {
-                            return user.category === category
-                        })
-                        setUsers(newFilterUsers)
-                    }
-                
+            if (category != "Toutes les catégories") {
 
-    }else{
-       await getUsers()
-    }
+                newFilterUsers = newUsers.filter((user: IUser) => {
+                    return user.category === category
+                })
+                setUsers(newFilterUsers)
+            }
+
+
+        } else {
+            await getUsers()
+        }
     }
 
     const setSearchNameUsers = async (searchTerm: string) => {
@@ -74,45 +75,47 @@ const UserList = () => {
 
     const [name, setName] = useState("");
     return (
-        <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <h1 className="text-3xl font-medium">Liste des collaborateurs</h1>
-            <div className="flex items-center px-4 py-3 bg-white border-b border-gray-200">
-                <input
-                    className="flex-1 px-4 py-2 mr-4 rounded-lg border border-gray-400"
-                    type="text"
-                    placeholder="Rechercher par nom..."
-                    onChange={e => setSearchNameUsers(e.target.value)}
-                />
-                <select
-                    className="px-4 py-2 mr-4 rounded-lg border border-gray-400"
-                    onChange={e => setSearchUser(e.target.value)}
-                >
-                 
+        <BaseLayout title={"Users"}>
+            <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <h1 className="text-3xl font-medium">Liste des collaborateurs</h1>
+                <div className="flex items-center px-4 py-3 bg-white border-b border-gray-200">
+                    <input
+                        className="flex-1 px-4 py-2 mr-4 rounded-lg border border-gray-400"
+                        type="text"
+                        placeholder="Rechercher par nom..."
+                        onChange={e => setSearchNameUsers(e.target.value)}
+                    />
+                    <select
+                        className="px-4 py-2 mr-4 rounded-lg border border-gray-400"
+                        onChange={e => setSearchUser(e.target.value)}
+                    >
+
                         <option>firstname</option>
                         <option>city</option>
 
-               
-                </select>
-                <select
-                    className="px-4 py-2 rounded-lg border border-gray-400"
-                    onChange={e => setSearchCategoryUsers(e.target.value)}
-                >
-                    <option>Toutes les catégories</option>
-                    <option>Marketing</option>
-                    <option>Technique</option>
-                    <option>Client</option>
-                </select>
-            </div>
-            <div className="flex flex-col items-center">
+
+                    </select>
+                    <select
+                        className="px-4 py-2 rounded-lg border border-gray-400"
+                        onChange={e => setSearchCategoryUsers(e.target.value)}
+                    >
+                        <option>Toutes les catégories</option>
+                        <option>Marketing</option>
+                        <option>Technique</option>
+                        <option>Client</option>
+                    </select>
+                </div>
+                <div className="flex flex-col items-center">
 
 
-                <div className="flex flex-wrap justify-center">
-                    {users.map((user) => (
-                        <UserCard key={user.id} user={user} />
-                    ))}
+                    <div className="flex flex-wrap justify-center">
+                        {users.map((user) => (
+                            <UserCard key={user.id} user={user}/>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </BaseLayout>
     )
 };
 
