8ebce0282b7eac830a21ab5364aef273bfe7fb52
regitraz2 <guillaume@subvitamine.com>
2023-02-07 14:37:01 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import BaseLayout from "@/components/BaseLayout";
-import axios from "axios";
-import * as process from "process";
 
 const Dashboard = () => {
-    const user = axios.get(process.env.NEXT_PUBLIC_API_URL + "/randomUser", {
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
-        }
-    }).then(res => res.data);
-
-    console.log(user);
+    // const user = axios.get(process.env.NEXT_PUBLIC_API_URL + "/randomUser", {
+    //     headers: {
+    //         'Authorization': `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+    //     }
+    // }).then(res => res.data);
+    //
+    // console.log(user);
 
     return (
         <>
