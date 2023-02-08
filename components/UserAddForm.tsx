6a7b67baa10e413c5b40b5d1d2e19008dae8c03e
regitraz2 <guillaume@subvitamine.com>
2023-02-08 10:45:27 +0100

    dfs

--- components/UserAddForm.tsx
+++ components/UserAddForm.tsx
@@ -102,7 +102,6 @@ const UserAddForm: FunctionComponent<IProps> = ({user, action}) => {
                                        className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-500 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                        id="isAdmin"/>
                             </label>
-                        </div>
                         <button type="submit"
                                 className="flex-1 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Submit
                         </button>
